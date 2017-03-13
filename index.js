81cbcf3b43ea045e8b33cf439de202c41dedc844
Mac <Mac@Macs-MacBook-Air.local>
2017-03-13 17:11:04 -0400

    Added required get, put, post, and delete functions.

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
 var express = require('express');
 var server = express();
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 8080;
 var mongoURI = process.env.MONGOURI || require('./secrets').mongoURI;
 
+//powerup -- middleware
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
+
 //connect to the database
 mongoose.connect(mongoURI);
-//Create the mongoose Scema
+//Create the mongoose Schema
 var foodSchema = mongoose.Schema({
   price: Number,
   category: String,
-  isGlutenFree: String,
+  isGlutenFree: Boolean,
   calories: Number
 });
 //Create the Mongoose Model
 var Food = mongoose.model('Food', foodSchema);
 //Testing database stuff
-// var donkey = new Animal({
-//   color: 'gray',
-//   size: 'MED',
-//   type: 'donkey',
-//   price: 180
-// });
-// donkey.save(function(err, data){
-//   if(err){
-//     console.log(err);
-//   } else {
-//     console.log(data);
-//   }
-// });
+ var cheese_sticks = new Food({
+   price: 8,
+   category: 'appetizer',
+   isGlutenFree: false,
+   calories: 480
+ });
+ cheese_sticks.save(function(err, data){
+   if(err){
+     console.log(err);
+   } else {
+     console.log(data);
+   }
+ });
 
 //GET /foods
 server.get('/foods', function(req, res){
@@ -62,25 +67,102 @@ server.get('/foods/:id', function(req, res){
   });
 });
 
-
 //GET /foods/category/:categoryName
-server.get('/foods/category/:categoryName', function(req, res){
-  Food.find({_id: req.params.id}, function(err, documents){
+ server.get('/foods/category/:categoryName', function(req, res){
+   Food.find({category: req.params.categoryName}, function(err, documents){
+     if(err){
+       res.status(500).json({
+        msg: err
+       });
+     } else {
+       res.status(200).json({
+         foods: documents
+       });
+     }
+   });
+ });
+
+ //GET /foods/price/:dollarAmount
+ server.get('/foods/price/:dollarAmount', function(req, res){
+   Food.find({price: req.params.dollarAmount}, function(err, documents){
+     if(err){
+       res.status(500).json({
+         msg: err
+       });
+     } else {
+       res.status(200).json({
+         foods: documents
+       });
+     }
+   });
+ });
+
+//POST /foods
+// server.post('/foods', function(req, res){
+//   res.send('it workss');
+// });
+
+//POST /foods
+server.post('/foods', function(req, res){
+  var food = new Food(req.body);
+  food.save(function(err, document){
     if(err){
       res.status(500).json({
-       msg: err
+        msg: err
+      });
+    }  else{
+      res.status(201).json({
+        msg: "Success"
+      });
+      }
+    });
+});
+
+//PUT /animals/:id
+server.put('/foods/:id', function(req, res){
+  Food.findOneAndUpdate({_id: req.params.id}, req.body, function(err, document){
+    if(err){
+      res.status(500).json({
+        msg: err
       });
     } else {
       res.status(200).json({
-        animals: documents
+        msg: "Successfully updated"
       });
     }
   });
 });
-//POST /animals
-//PUT /animals/:id
-//DELETE /animals/:id
+
+//DELETE /foods/:id
+server.delete('/foods/:id', function(req, res){
+  Food.remove({_id: req.params.id}, function(err, document){
+    if(err){
+      res.status(500).json({
+        msg: err
+      });
+    } else {
+      res.status(200).json({
+        msg: 'Successfully deleted'
+      });
+    }
+  });
+});
+
+//DELETE /foods/category/:category
+server.delete('/foods/category/:categoryName', function(req, res){
+  Food.remove({category: req.params.categoryName}, function(err, document){
+    if(err){
+      res.status(500).json({
+        msg: err
+      });
+    } else {
+      res.status(200).json({
+        msg: 'Successfully deleted category'
+      });
+    }
+  });
+});
 
 server.listen(port, function(){
   console.log('Now listening on port...', port);
-})
+});
